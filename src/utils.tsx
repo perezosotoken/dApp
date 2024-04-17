d0b3d8d7b8a364e219900ab92807ec2b87d64ed8
alberthaptic <albert@haptic.finance>
2024-04-17 10:35:07 -0400

    commify util

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,6 +1,7 @@
 
 
 export const commify = (number) => {
+  if (number === undefined) return "";
     let numStr = number.toString();
     let [integerPart, decimalPart] = numStr.split(".");
     integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
