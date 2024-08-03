039843c31e2f10635139b36d1cdf2470cd169fb0
alberthaptic <albert@haptic.finance>
2024-08-02 20:20:35 -0400

    edit APR

--- src/pages/Staking.tsx
+++ src/pages/Staking.tsx
@@ -179,7 +179,7 @@ const Staking: React.FC = () => {
     
     const calculateAPR = async () => {
       if (stakingContractBalance) {
-        const weeklyRewards = 400_000_000_000;
+        const weeklyRewards = 100_000_000_000;
 
         const stakingContractBalanceReadable = formatEther(stakingContractBalance); 
         const balanceWithoutRewards = Number(stakingContractBalanceReadable) - weeklyRewards;
@@ -197,7 +197,7 @@ const Staking: React.FC = () => {
 
     const calculateAPRLP = async () => {
       if (stakingContractBalance) {
-        const weeklyRewards = 100_000_000_000 * Number(tokenPrice);
+        const weeklyRewards = 400_000_000_000 * Number(tokenPrice);
 
         const stakingContractBalanceReadable = formatEther(totalSupplyLP); 
  
