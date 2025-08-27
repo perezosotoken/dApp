2feca5637c7bb30c3bbb8f5e5944315529762936
alberthaptic <albert@haptic.finance>
2025-08-27 10:59:47 -0400

    disable btn conditionally

--- src/components/StakeControls.tsx
+++ src/components/StakeControls.tsx
@@ -19,18 +19,18 @@ function StakingTable({amountToStake, przsBalance, lpTokenBalance, sideButtonsGr
       <VStack>
         <Box mt={isMobile ? 0 : 10}>
         <HStack>
-          <Button isDisabled={lpTokenBalance == 0 && przsBalance == 0} size={sideButtonsGroupSize} borderRadius={10} mt={5} onClick={() => handleStakeAll("25")}>25%</Button>&nbsp;
-          <Button isDisabled={lpTokenBalance == 0 && przsBalance == 0} size={sideButtonsGroupSize} borderRadius={10} mt={5} onClick={() => handleStakeAll("50")}>50%</Button>&nbsp;
+          <Button isDisabled={true} size={sideButtonsGroupSize} borderRadius={10} mt={5} onClick={() => handleStakeAll("25")}>25%</Button>&nbsp;
+          <Button isDisabled={true} size={sideButtonsGroupSize} borderRadius={10} mt={5} onClick={() => handleStakeAll("50")}>50%</Button>&nbsp;
           </HStack>
         <HStack>
-        <Button isDisabled={lpTokenBalance == 0 && przsBalance == 0} size={sideButtonsGroupSize} borderRadius={10} mt={5} onClick={() => handleStakeAll("75")}>75%</Button>&nbsp;
-        <Button isDisabled={lpTokenBalance == 0 && przsBalance == 0} border="4px solid green" size={sideButtonsGroupSize} borderRadius={10} mt={5} onClick={() => handleStakeAll("100")}>MAX</Button>&nbsp;
+        <Button isDisabled={true} size={sideButtonsGroupSize} borderRadius={10} mt={5} onClick={() => handleStakeAll("75")}>75%</Button>&nbsp;
+        <Button isDisabled={true} border="4px solid green" size={sideButtonsGroupSize} borderRadius={10} mt={5} onClick={() => handleStakeAll("100")}>MAX</Button>&nbsp;
         </HStack>      
         </Box>
 
         {isMobile ? 
           <Button 
-          isDisabled={amountToStake == 0}
+          isDisabled={true}
           width={"120px"} 
           style={{ border:"1px solid white", borderRadius:"10px"}}
           onClick={() => approve()}
